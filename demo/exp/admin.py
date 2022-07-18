920ab9a3f5b096f31ecaf0da332a4b5338987e2e
manvirajawat <manvi18rajawat@gmail.com>
2022-07-18 16:41:23 +0530

    added text.py

--- demo/exp/admin.py
+++ demo/exp/admin.py
@@ -1,7 +1,11 @@
+"""
+Admin for demo project.
+Register models in admin of demo project.
+"""
+
 from django.contrib import admin
 from exp.models import Post, Comment
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Comment)
-
--- demo/exp/models.py
+++ demo/exp/models.py
@@ -1,13 +1,31 @@
+"""
+Creating models Post and Comment.
+"""
+
 from django.db import models
 
 class Post(models.Model):
+    """
+    Parameters
+    ----------
+    created_on for timestamp
+    title for post titile
+    text for post description
+    """
     created_on = models.DateTimeField(auto_now_add=True, null=True)
     title = models.CharField(max_length=255)
     text = models.TextField()
 
 class Comment(models.Model):
+    """
+    Parameters
+    ----------
+    created_on
+    author_name
+    author_email
+    text
+    """
     created_on = models.DateTimeField(auto_now_add=True)
     author_name = models.CharField(max_length=255)
     author_email = models.EmailField()
     text = models.TextField()
-
